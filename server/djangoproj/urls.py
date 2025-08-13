6b6639874440239c1dbb2f18c92e59b672253b33
magnusAchor <rresha160@gmail.com>
2025-08-13 17:20:58 -0400

    retry

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -29,5 +29,7 @@ urlpatterns = [
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html"))
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
